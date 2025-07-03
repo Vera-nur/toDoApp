36e5761ceadf5be7f930ea4ef3aeb0282e53d125
Vera <135285088+Vera-nur@users.noreply.github.com>
2025-07-03 10:21:56 +0300

    Initial commit: Added basic ToDoApp project structure

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Vera Nur on 3.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
